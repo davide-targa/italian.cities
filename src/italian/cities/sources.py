dda05053ebed5106a4b4efd17a90cd7c0cd1ea5d
Davide Targa <davide.targa@gmail.com>
2020-04-27 14:50:42 +0200

    Fix source object.

--- src/italian/cities/sources.py
+++ src/italian/cities/sources.py
@@ -9,19 +9,25 @@ class ItalianCities(object):
 
     vocabulary = SimpleVocabulary((
         SimpleTerm(u'Bologna', 'bologna', u'Bologna'),
+        SimpleTerm(u'Roma', 'roma', u'Roma'),
+        SimpleTerm(u'Milano', 'milano', u'Milano'),
         SimpleTerm(u'Palermo', 'palermo', u'Palermo'),
         SimpleTerm(u'Sorrento', 'sorrento', u'Sorrento')))
 
     def __init__(self, context):
         self.context = context
 
-    __contains__ = vocabulary.__contains__
-    __iter__ = vocabulary.__iter__
-    getTerm = vocabulary.getTerm
-    getTermByToken = vocabulary.getTermByToken
+    def __contains__(self, value):
+        return self.vocabulary.__contains__(value)
+
+    def getTerm(self, value):
+        return self.vocabulary.getTerm(value)
+
+    def getTermByToken(self, value):
+        return self.vocabulary.getTermByToken(value)
 
     def search(self, query_string):
-        return [v for v in self if query_string.lower() in v.value.lower()]
+        return [v for v in self.vocabulary if query_string.lower() in v.value.lower()]
 
 
 @implementer(IContextSourceBinder)
